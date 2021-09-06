3b1adff9214d62bccd2a8a5af0cd3aeb0741c1eb
ciprodakama <teli.uni00@gmail.com>
2021-09-06 11:14:29 +0200

    Changed Font & Added Colors

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 // Modal Image Gallery
 function onClick(element) {
     document.getElementById("img01").src = element.src;
